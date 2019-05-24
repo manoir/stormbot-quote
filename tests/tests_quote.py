2b0f7feb019c449cfba640ecdc71d5629ebf07b5
Paul Fariello <paul@fariello.eu>
2019-05-24 23:31:06 +0200

    Test with patched StormBot not with patched FakeBot

--- tests/tests_quote.py
+++ tests/tests_quote.py
@@ -1,28 +1,51 @@
 import sys
 import unittest
 
-from stormbot import mock, bot
+from unittest.mock import patch
+from stormbot import mock
 from stormbot_quote import Quote
 
 
 class TestQuoteCommand(unittest.TestCase):
-    @mock.bot(['quote'])
-    @mock.storage({'quotes': {'author': ['quote']}})
-    def test_one_quote(self, stdout):
-        # When
-        bot.main(Quote)
+    def test_no_quote(self):
+        # Given
+        with patch('stormbot_quote.Storage', mock.Storage()) as storage:
+            bot = mock.bot(Quote)
+
+            # When
+            bot.command("stormbot: quote")
 
         # Then
-        self.assertEqual(stdout.getvalue(), "author \"quote\"\n")
+        bot.send_message.assert_called_with(mbody="We don't have any quote yet, feel free to add some.",
+                                            mto=bot.room, mtype="groupchat")
 
-    @mock.bot(['quote', 'author', 'quote'])
-    @mock.storage({'quotes': {}})
-    def test_add_quote(self, stdout):
-        # When
-        bot.main(Quote)
+    def test_one_quote(self):
+        # Given
+        with patch('stormbot_quote.Storage', mock.Storage()) as storage:
+            storage['quotes'] = {'author': ['quote']}
+            bot = mock.bot(Quote)
+
+            # When
+            bot.command("stormbot: quote")
 
         # Then
-        self.assertEqual(stdout.getvalue(), "Your words are now engraved in the stones\n")
+        bot.send_message.assert_called_with(mbody="author \"quote\"",
+                                            mto=bot.room, mtype="groupchat")
+
+    def test_add_quote(self):
+        # Given
+        with patch('stormbot_quote.Storage', mock.Storage()) as storage:
+            bot = mock.bot(Quote)
+
+            # When
+            bot.command("stormbot: quote author \"My wonderful quote\"")
+
+        # Then
+        bot.send_message.assert_called_with(mbody="Your words are now engraved in the stones",
+                                            mto=bot.room, mtype="groupchat")
+        self.assertIn('quotes', storage)
+        self.assertIn('author', storage['quotes'])
+        self.assertEqual(storage['quotes']['author'], ["My wonderful quote"])
 
 
 if __name__ == '__main__':
